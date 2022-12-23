44b130d65b826133cfcd5a1edb1033bb8e64b909
Clizan Willian <clizan-ws@hotmail.com>
2022-12-23 11:46:49 -0300

    versão cinco

--- view/cadastro.php
+++ view/cadastro.php
@@ -16,27 +16,27 @@
   <link rel="stylesheet" href="./css/estilo.css">
 
  </head>
- <body>
+ <body class="bodyMenu">
 
   <div class="container" style="margin-top: 50px; border-radius: 5px; height: 89vh">
-  
-   <label class="titulo">Código</label>
     
-   <div class="row mb-3">
-       
+   <div class="row layoutCadastrar">
+      
     <div class="col-sm-4">
+   
+     <label class="titulo">Código</label>
    
      <input type="text" class="form-control" id="codigo">
     
     </div>
   
    </div>
-  
-   <label class="titulo">Descrição do Produto</label>
-    
-   <div class="row mb-3">
      
+   <div class="row layoutCadastrar">
+       
     <div class="col-sm-12">
+    
+    <label class="titulo">Descrição do Produto</label>
  
      <input type="text" class="form-control" id="produto">
   

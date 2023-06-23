e8540a604ad207fdb978da9c6b4ddf72c7fedfec
Clizan Willian <31556355+Clizan@users.noreply.github.com>
2023-06-23 17:20:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php 
- #incluindo a classe de conexão
- include_once './model/conexao.php'; 
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,7 +5,7 @@
  <title>Login</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css"  href="./view/css/estilo.css" />
+  <link rel="stylesheet" type="text/css"  href="../css/estilo.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
  </head>
@@ -33,14 +27,14 @@
    
      <div class="user-box">
     
-      <input type="text" id="user" required="">    
+      <input type="text" id="usuario" required="">    
       <label>Usuário</label>
     
      </div>
     
      <div class="user-box">
     
-      <input type="password" id="pass" required="">
+      <input type="password" id="senha" required="">
       <label>Senha</label>
     
      </div>
@@ -57,9 +51,13 @@
      
      </div>
 
-     <br />
+     <span id="carregandoInformacao"></span>
     
+     <br />
+
      <button class="btn btn-success" id="acessar">Acessar</button>
+
+     <span id="resultado"></span>
     
     </div>
    
@@ -71,6 +69,8 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><!--Bibliotecas Ajax -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="sweetalert2.all.min.js"></script>
   <script src="./view/js/script.js"></script><!--Script for page timeout-->
 
  </body>

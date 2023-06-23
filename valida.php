a894a9b793a3ddfcb71d103ea2b44573a3df9e28
Clizan Willian <31556355+Clizan@users.noreply.github.com>
2023-06-23 17:14:12 -0300

    Update valida.php

--- valida.php
+++ valida.php
@@ -1,32 +1,33 @@
 <?php
-
- #Acá yo estoy hacendo un include da clase conexión
- include_once './model/conexao.php';
-
+ 
  #Acá estamos checkando se la variable tuve un POST 
- if(isset($_POST['user']) && !empty($_POST['user'])){
+ if(isset($_POST['usuario']) && !empty($_POST['senha'])){
 
   #Variable con su valor pasado por Javascript
-  $getUser = $_POST['user'];
-  $getPass = $_POST['pass'];
+  $usuario = $_POST["usuario"];
+  $senha = $_POST["senha"];
 
+  #Variable responsable por tener el valor de retorno para la pantalla index.php
+  $retorno = null;
+  
   #Variable que hace la consulta en banco de datos para identificar se existe un registro con esas informaciones para hacer el acceso a lá aplicacíon
   $select = $conexao->query("SELECT usuario, senha FROM loja.ctrl_usuarios WHERE usuario = '$getUser' AND senha = '$getPass'");
-  
+    
+  #cuantidad de datos encontrados en banco de datos co eses valores
   if($select->num_rows > 0){
 
-   echo $retorna = 1; 
-
-   /*sleep(3);  
-   header("Location: ./view/menu.php");
- */
-  }else{
+   $retorno .= 1; 
    
-   sleep(3); 
-   header("Location: index.php");
-
-  }
+  }else{
+     
+   $retorno .= 0; 
   
+  }
+
+  #aca estoy haciendo el retorno de valor para la pantalla index
+  $resultado = $retorno;
+  echo json_encode($resultado);
+    
  }
- 
+
 ?>

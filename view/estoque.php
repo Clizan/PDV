69d999edece282288dc70c58c33e7ec5506f871d
Clizan Willian <31556355+Clizan@users.noreply.github.com>
2023-06-29 17:51:53 -0300

    Create estoque.php

---
+++ view/estoque.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <title>Estoque</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-sm-4">
+
+                <label><img src="./img/home.png"> Home > Cadsatro de Produtos </label>
+
+            </div>
+
+            <div class="col-sm-6">
+
+            </div>
+
+            <div class="col-sm-2">
+
+                <button class="btn btn-light">Voltar</button>
+                <button class="btn btn-light">Ajuda</button>
+
+            </div>
+
+        </div>
+
+        <hr>
+
+        <div class="row">
+
+            <div class="col-sm-4">
+
+                <button class="btn btn-light"><img src="./img/home.png"> Incluir</button>
+
+            </div>
+
+            <div class="col-sm-4">
+
+            </div>
+
+            <div class="col-sm-4">
+
+                <select class="form-select" aria-label="Disabled select example">
+
+                    <option selected> - </option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+
+                </select>
+
+                <input type="text"><img src="./img/pesquisar">
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>

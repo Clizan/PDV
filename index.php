1ddf090649efe5a5fd62f77b763fe5f31f2c2876
Clizan Willian <clizan-ws@hotmail.com>
2024-01-25 23:56:50 -0300

    add of the class bootstrapp

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-AU-Copatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <title>Login</title>
     </head>
 
     <body>
 
-        <div id="login">
+        <div class="container-fluid" id="login">
 
             <div class="caixa">
 

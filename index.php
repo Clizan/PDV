487004b4e51a4da48cb38f59815fedfde168652a
Clizan Willian <clizan-ws@hotmail.com>
2024-01-30 22:50:19 -0300

    add font Roboto in archive index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-AU-Copatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <ling rel="stylesheet" href="./view/css/stule.css"
         <title>Login</title>
     </head>
 

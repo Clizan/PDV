7d7f2e3a683dfe7cb1227ab8e20114b7bd8c8893
Clizan Willian <clizan-ws@hotmail.com>
2024-02-21 22:29:47 -0300

    add script in javascript for to make the integration of page index and validation

--- index.php
+++ index.php
@@ -38,4 +38,9 @@
 
     </body>
 
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="./js/script.js"></script><!--Script for page timeout-->
+
 </html> 
